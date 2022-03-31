cdc92fd9aefce53d0703e5297af96f597fc28dca
Wingchi Tam <41390970+wingchi-tam@users.noreply.github.com>
2022-03-30 21:59:57 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 const schedule = {
   "title": "CS Courses for 2018-2019",
@@ -56,12 +57,33 @@ const Course = ({ course }) => (
   </div>
 );
 
-const App = () =>  (
-  <div className="container">
-    <Banner title={ schedule.title } />
-    <CourseList courses={ schedule.courses } />
-  </div>
-);
+
+
+const App = () => {
+  const [schedule, setSchedule] = useState();
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, []);
+
+  if (!schedule) return <h1>Loading schedule...</h1>;
+
+  return (
+    <div className="container">
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </div>
+  );
+};
+
+
 
 
 export default App;
