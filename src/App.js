506abcbd17dbfa85a48b48f46ab17f8cd746f3b2
Wingchi Tam <41390970+wingchi-tam@users.noreply.github.com>
2022-03-31 15:16:01 -0500

    modularize

--- src/App.js
+++ src/App.js
@@ -1,99 +1,14 @@
-
 import './App.css';
 import React, { useState, useEffect } from 'react';
-
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "id" : "F101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "id" : "F110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "id" : "S313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "id" : "S314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
+import CourseList from './components/CourseList.js';
 
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
 );
 
-const CourseList = ({ courses }) => {
-  const [term, setTerm] = useState('Fall');
-  const [selected, setSelected] = useState([]);
-  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
-  
-  return (
-    <>
-      <TermSelector term={term} setTerm={setTerm} />
-      <div className="course-list">
-      { 
-        termCourses.map(course =>
-          <Course key={ course.id } course={ course }
-            selected={selected} setSelected={ setSelected } 
-          />) 
-      }
-      </div>
-    </>
-  );
-};
-
-const TermButton = ({term, setTerm, checked}) => (
-  <>
-    <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
-      onChange={() => setTerm(term)} />
-    <label class="btn btn-success m-1 p-2" htmlFor={term}>
-    { term }
-    </label>
-  </>
-);
-
-const TermSelector = ({term, setTerm}) => (
-  <div className="btn-group">
-  { 
-    Object.values(terms).map(value => (
-      <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
-    ))
-  }
-  </div>
-);
-
-const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
-
-const getCourseTerm = course => (
-  terms[course.id.charAt(0)]
-);
-
-const getCourseNumber = course => (
-  course.id.slice(1, 4)
-);
-
-const toggle = (x, lst) => (
-  lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
-);
-
-
-const hasConflict = (course, selected) => (
-  selected.some(selection => courseConflict(course, selection))
-);
-
 const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
 
-const timeParts = meets => {
+export const timeParts = meets => {
   const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
   return !match ? {} : {
     days,
@@ -118,43 +33,6 @@ const addScheduleTimes = schedule => ({
   courses: mapValues(addCourseTimes, schedule.courses)
 });
 
-const days = ['M', 'Tu', 'W', 'Th', 'F'];
-
-const daysOverlap = (days1, days2) => ( 
-  days.some(day => days1.includes(day) && days2.includes(day))
-);
-
-const hoursOverlap = (hours1, hours2) => (
-  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
-);
-
-const timeConflict = (course1, course2) => (
-  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
-);
-
-const courseConflict = (course1, course2) => (
-  getCourseTerm(course1) === getCourseTerm(course2)
-  && timeConflict(course1, course2)
-);
-
-const Course = ({ course, selected, setSelected }) => {
-  const isSelected = selected.includes(course);
-  const isDisabled = !isSelected && hasConflict(course, selected);
-  const style = {
-    backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
-  };
-  return (
-    <div className="card m-1 p-2" 
-      style={style}
-      onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}>
-      <div className="card-body">
-        <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
-        <div className="card-text">{ course.title }</div>
-        <div className="card-text">{ course.meets }</div>
-      </div>
-    </div>
-  );
-};
 
 const App = () => {
   const [schedule, setSchedule] = useState();
@@ -179,8 +57,5 @@ const App = () => {
     </div>
   );
 };
-
-
-
 
 export default App;
